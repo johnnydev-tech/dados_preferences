ea58309d12c5633e5edd146d7065a92efb5648ab
Johnny Freire <johnnyfreirejh@hotmail.com>
2021-02-24 18:56:22 -0300

    Initial commit

---
+++ android/app/src/main/java/br/com/dropc/dados_preferences/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.dropc.dados_preferences;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:dados_preferences/views/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
